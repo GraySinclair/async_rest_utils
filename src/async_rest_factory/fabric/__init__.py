9d20f88f60501fde31ea9841f37d2d93099b6b7b
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2026-07-09 15:22:34 -0500

    Update import paths in __init__.py

--- src/async_rest_factory/fabric/__init__.py
+++ src/async_rest_factory/fabric/__init__.py
@@ -1,6 +1,6 @@
-from async_rest_utils.fabric.config_loader import load_rest_configs
-from async_rest_utils.fabric.key_vault import get_key_vault_secret
-from async_rest_utils.fabric.lakehouse_files import write_rows_to_lakehouse_json
+from async_rest_factory.fabric.config_loader import load_rest_configs
+from async_rest_factory.fabric.key_vault import get_key_vault_secret
+from async_rest_factory.fabric.lakehouse_files import write_rows_to_lakehouse_json
 
 __all__ = [
     "load_rest_configs",
